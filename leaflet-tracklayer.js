dc956a6c4fdd01e627c1241ef4bc05478856a7b9
Tim Leerhoff <tleerhof@web.de>
2015-09-22 20:46:41 +0200

    ...

--- leaflet-tracklayer.js
+++ leaflet-tracklayer.js
@@ -1,12 +1,10 @@
-var L.TrackLayer = function () {
-  var mmsiToMarker = {};
+var L.TrackLayer = function() {
   var aislayer = L.featureGroup([]);
 
-  var addTrackMarker = function(llPoint) {
+  this.addTrackMarker = function(llPoint) {
     var marker = L.trackSymbol(llPoint);
     marker.addTo(aislayer);
   };
-
 };
 
 var L.trackLayer = function() {
