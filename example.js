477a390e02360ca15a16a3e59999298f1abe567e
Tim Leerhoff <tleerhof@web.de>
2016-02-16 17:32:30 +0100

    Added example with leaflet.

--- example.js
+++ example.js
@@ -4,9 +4,13 @@ var express = require('express');
 var http = require('http');
 var app = express();
 
-// this will make Express serve your static files
 var serverPath = __dirname + '/public';
+var leafletFile = __dirname + '/leaflet-tracksymbol.js';
+
 app.use(express.static(serverPath));
+app.get('/leaflet-tracksymbol.js', function(req, res) {
+	res.sendFile(leafletFile);
+});
 
 var httpServer = http.createServer(app).listen(PORT);
 console.log('Http-server started in port ' + PORT);
